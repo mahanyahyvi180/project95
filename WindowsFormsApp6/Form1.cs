f3ee2ca5e173c7cf3212cb7abc6fc6dce0b4a44e
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-14 20:05:17 +0330

    Add files via upload

---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int sum;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            sum += x;
+            lable3.Text = sum.ToString();
+            lable3.Text = "";
+            lable3.Focus();
+        }
+    }
+}
